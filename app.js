d93e96c07296b9ea6f54aeabc65a2258ed3145af
Tushar <tusharchawla2002@gmail.com>
2024-12-04 15:07:39 +0530

    Added frontend origin url

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ let app = express();
 app.use(
     cors({
       // origin: ["http://localhost:5173"],
-      origin: ["https://tushar-30.github.io/Todo-List/"],
+      origin: ["https://tushar-30.github.io/"],
       methods: ["GET", "PUT", "DELETE", "POST","PATCH"],
       credentials: true,
     })
